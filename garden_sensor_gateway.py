5bf5c33612cd3cac7dbe00e5efed431e14b7d03f
sgeorge-nb <122819133+sgeorge-nb@users.noreply.github.com>
2023-05-21 20:14:30 +0200

    Update code
    - Add location tag to each data
    
    - Modify telegraf config to accept string tags

--- garden_sensor_gateway.py
+++ garden_sensor_gateway.py
@@ -4,7 +4,6 @@ import random
 
 from kafka import KafkaProducer
 
-
 def random_temp_cels():
     return round(random.uniform(-10, 50), 1)
 
@@ -17,6 +16,9 @@ def random_wind():
 def random_soil():
     return round(random.uniform(0, 100), 1)
 
+def random_location():
+    return random.choice(["fulda", "kassel", "frankfurt", "münchen"])
+
 def get_json_data():
     data = {}
 
@@ -24,17 +26,19 @@ def get_json_data():
     data["humidity"] = random_humidity()
     data["wind"] = random_wind()
     data["soil"] = random_soil()
+    data["location"] = random_location()
 
     return json.dumps(data) 
 
 def main():
     producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
 
-    for _ in range(20000):
+    for _ in range(500000):
+    # for _ in range(500):
         json_data = get_json_data()
         producer.send('garden_sensor_data', bytes(f'{json_data}','UTF-8'))
         print(f"Sensor data is sent: {json_data}")
-        time.sleep(5)
+        time.sleep(0.001 / 3)
 
 
 if __name__ == "__main__":
